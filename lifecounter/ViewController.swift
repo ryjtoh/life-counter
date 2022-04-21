c1a042b8e3f3ef0fec9d093054469f7581a7ac1f
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2022-04-21 14:35:54 -0700

    Initial Commit

---
+++ lifecounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounter
+//
+//  Created by Ryan Oh on 4/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
